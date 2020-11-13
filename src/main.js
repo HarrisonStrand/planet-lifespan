7a9e2ce94b8233f9711b036aff4bfd600cd18837
HarrisonStrand <harrisonstrand@gmail.com>
2020-11-13 15:56:19 -0800

    add name change to test file

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { Age } from './js/ageConversions.js';
+import Age from './js/ageConversions.js';
 
 $(document).ready(function() {
   $("button#calculate").click(function(event) {
