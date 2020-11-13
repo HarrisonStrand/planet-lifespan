674d5b64a325cc3d45eb8ec42ba1252ad6cc4513
HarrisonStrand <harrisonstrand@gmail.com>
2020-11-13 10:10:54 -0800

    purposeful pass for object construction

--- _tests_/earthConversions.test.js
+++ _tests_/earthConversions.test.js
@@ -7,8 +7,8 @@ describe('Earth', () => {
   earth = new Earth(1, 365, 8760);
 });
   test('should create a new object Earth with properties', () => {
-      expect(earth.year).toEqual(1);
+      expect(earth.years).toEqual(1);
       expect(earth.days).toEqual(365);
-      expect(earth.ours).toEqual(8760);
+      expect(earth.hours).toEqual(8760);
     });
 });
