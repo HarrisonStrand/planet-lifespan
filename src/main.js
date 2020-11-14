5d89c411fd0f91d591f4c55abda856629a7e718f
HarrisonStrand <harrisonstrand@gmail.com>
2020-11-13 16:33:01 -0800

    add css styling for project

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import './css/styles.css';
 import Age from './js/ageConversions.js';
 
 $(document).ready(function() {
+  let planet1 = "Mercury";
+  let litAge = (age.mercuryConv());
+  let litStr = 'Your age in ${planet1} is ${litStr}!!';
   $("button#calculate").click(function(event) {
     $("#results").text(strLit);
     event.preventDefault();
